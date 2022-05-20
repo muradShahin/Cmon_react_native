2920b640e00b698abb9cf2779792f6c965ff912f
muradshahin <shahinmurad43@gmail.com>
2022-05-20 14:31:41 +0300

    MultiLinear charts for call screen

--- src/Screens/CallScreen.js
+++ src/Screens/CallScreen.js
@@ -7,7 +7,7 @@ import {
     StackedBarChart
   } from "react-native-chart-kit";
 
-  import React,{useState,useEffect} from "react";
+import React,{useState,useEffect} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { Dimensions } from "react-native";
 
@@ -38,16 +38,36 @@ const data2 = {
            380,
            250,
            430
-          ]
+          ],color: (opacity = 1) => `#9cff57`
+        } ,
+        {
+
+          data: [
+            91,
+            21,
+            120,
+            50,
+            11
+           ],color: (opacity = 1) => `#039be5`
+
+        },{
+
+          data: [
+            250,
+            136,
+            200,
+            145,
+            80
+           ],color: (opacity = 1) => `#ff5f52`
         }
       ]
     }}
     width={Dimensions.get("window").width} // from react-native
-    height={Dimensions.get("window").height/2.5}
+    height={Dimensions.get("window").height/2.25}
   
    // yAxisInterval={1} // optional, defaults to 1
     chartConfig={{
-      backgroundColor: "#2e7d32",
+      backgroundColor: "#a5d6a7",
       backgroundGradientFrom: "#60ad5e",
       backgroundGradientTo: "#60ad5e",
       decimalPlaces: 2, // optional, defaults to 2dp
@@ -71,7 +91,7 @@ const data2 = {
 
 
 
-<BarChart
+{/* <BarChart
   style={styles.chart_bar2}
   data={data2}
   width={Dimensions.get("window").width}
@@ -89,7 +109,70 @@ const data2 = {
   }}
 
 
-/>
+/> */}
+
+
+<LineChart
+    data={{
+      labels: ["00:47", "00:54", "01:01", "01:08", "01:16"],
+      datasets: [
+        {
+          data: [
+            10,
+            10,
+            10,
+            23,
+            13
+          ],color: (opacity = 1) => `#870000`
+        } ,
+        {
+
+          data: [
+           4,
+           4,
+           6,
+           9,
+           3
+           ],color: (opacity = 1) => `#ffeb3b`
+
+        },{
+
+          data: [
+           0,
+           0,
+           3,
+           2,
+           0
+           ],color: (opacity = 1) => `#9cff57`
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={Dimensions.get("window").height/2}
+  
+   // yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#0277bd",
+      backgroundGradientFrom: "#004c8c",
+      backgroundGradientTo: "#0277bd",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      margin:0,
+      borderRadius: 0
+    }}
+  />
 
 
 </View>
